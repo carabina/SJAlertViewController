e7f0455773dcec5fdbb0ea3e15b26dfdc0faf4be
Jaesun <1016097566@qq.com>
2018-12-03 11:21:10 +0800

    no message

---
+++ SJAlertViewController/SJAlertViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SJAlertViewController
+//
+//  Created by Mac on 2018/12/3.
+//  Copyright © 2018年 S.J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
